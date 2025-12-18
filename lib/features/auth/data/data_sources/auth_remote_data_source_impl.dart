cd56b0681fea132b494364a1e111175903c7a00c
Sholihul Fadjri Triwibowo <88471883+Fdjri@users.noreply.github.com>
2025-12-18 10:03:09 +0700

    add auth feature

---
+++ lib/features/auth/data/data_sources/auth_remote_data_source_impl.dart
@@ -0,0 +1,13 @@
+import 'auth_remote_data_source.dart';
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  const AuthRemoteDataSourceImpl();
+
+  Future<T> _run<T>(Future<T> Function() function) async {
+    try {
+      return await function();
+    } catch (e) {
+      throw e;
+    }
+  }
+}
